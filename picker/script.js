a78afd82d808284750dd214a5f73e2bc678e5e2f
Marek Dorda <marek.dorda@windy.com>
2022-06-27 13:07:06 +0200

    Update picker example

--- picker/script.js
+++ picker/script.js
@@ -6,32 +6,35 @@ const options = {
 };
 
 windyInit(options, windyAPI => {
-    const { picker, utils, broadcast } = windyAPI;
+    const { picker, utils, broadcast, store } = windyAPI;
 
-    picker.on('pickerOpened', latLon => {
-        // picker has been opened at latLon coords
-        console.log(latLon);
-
-        const { lat, lon, values, overlay } = picker.getParams();
+    picker.on('pickerOpened', ({ lat, lon, values, overlay }) => {
         // -> 48.4, 14.3, [ U,V, ], 'wind'
-        console.log(lat, lon, values, overlay);
+        console.log('opened', lat, lon, values, overlay);
 
         const windObject = utils.wind2obj(values);
         console.log(windObject);
     });
 
-    picker.on('pickerMoved', latLon => {
+    picker.on('pickerMoved', ({ lat, lon, values, overlay }) => {
         // picker was dragged by user to latLon coords
-        console.log(latLon);
+        console.log('moved', lat, lon, values, overlay);
     });
 
     picker.on('pickerClosed', () => {
         // picker was closed
     });
 
+    store.on('pickerLocation', ({ lat, lon }) => {
+        console.log(lat, lon);
+
+        const { values, overlay } = picker.getParams();
+        console.log('location changed', lat, lon, values, overlay);
+    });
+
     // Wait since wather is rendered
     broadcast.once('redrawFinished', () => {
-        picker.open({ lat: 48.4, lon: 14.3 });
         // Opening of a picker (async)
+        picker.open({ lat: 48.4, lon: 14.3 });
     });
 });
